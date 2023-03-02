ac25b66767093d87e0c2121b01e75a4151db875d
Viktor <lulzsec123@yahoo.com>
2023-03-02 11:03:23 +0200

    firebase-crud-create-finish

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import { useState, useEffect } from 'react';
 import {db} from './firebase-config';
-import {collection, getDocs} from 'firebase/firestore';
+import {addDoc, collection, getDocs} from 'firebase/firestore';
 
 
-function App() {  
+function App() {
+  const [getNewName,setNewName] = useState("");  
   const [users, setUser] = useState([]);
   const usersCollectionRef=collection(db,'users');
+
+  const createUser = async() => {
+    await addDoc(usersCollectionRef, {name:getNewName});
+  }
 
   useEffect(()=>{
     const getUsers = async()=>{
@@ -18,6 +23,8 @@ function App() {
   }, [])
   return (
     <div className="App">
+      <input placeholder='Name...' onChange={(event)=>{setNewName(event.target.value)}}></input>
+      <button onClick={createUser}>Submit</button>
       {users.map((user)=>{return <div><h1 key={user.id}>Name:{user.name}</h1></div>})}
     </div>
   );
