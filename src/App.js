caafafc5275d8266db20bb84579a39479e42dc26
Viktor <lulzsec123@yahoo.com>
2023-03-11 17:20:15 +0200

    initialize-authentication

--- src/App.js
+++ src/App.js
@@ -1,42 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
-import { useState, useEffect } from 'react';
-import {db} from './firebase-config';
-import {addDoc, collection, getDocs, updateDoc,doc, deleteDoc} from 'firebase/firestore';
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Crud from "./CRUD";
+import Auth from "./Authentication";
 
 function App() {
-  const [getNewName,setNewName] = useState("");  
-  const [users, setUser] = useState([]);
-  const usersCollectionRef=collection(db,'users');
-
-  const createUser = async() => {
-    await addDoc(usersCollectionRef, {name:getNewName});
-  }
-  const editUser = async(id) => {
-    const userDoc = doc(db,"users",id);
-    const newName = {name:getNewName};
-    await updateDoc(userDoc, newName);
-  }
-  const deleteUser = async(id) => {
-    const userDoc = doc(db,"users",id);
-    await deleteDoc(userDoc);
-  }
-
-  useEffect(()=>{
-    const getUsers = async()=>{
-      const data = await getDocs(usersCollectionRef);
-      setUser(data.docs.map((doc) => ({...doc.data(), id:doc.id})))
-    }
-    getUsers();
-  }, [])
-  return (
-    <div className="App">
-      <input placeholder='Name...' onChange={(event)=>{setNewName(event.target.value)}}></input>
-      <button onClick={createUser}>Submit</button>
-      {users.map((user)=>{return <div><h1 key={user.id}>Name:{user.name}</h1> <button onClick={()=>{editUser(user.id)}}>Edit</button> <button onClick={()=>{deleteUser(user.id)}}>Delete</button></div>})}
-    </div>
-  );
+  return(
+    <BrowserRouter>
+      <Routes>
+          <Route path='/crud' element={<Crud />} />
+          <Route path='/auth' element={<Auth />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
---
+++ src/Authentication.js
@@ -0,0 +1,38 @@
+import { useState, useEffect } from 'react';
+import { db } from './firebase-config';
+import { addDoc, collection, getDocs, updateDoc, doc, deleteDoc } from 'firebase/firestore';
+
+function Authentication() {
+    const [getNewName, setNewName] = useState("");
+    const [users, setUser] = useState([]);
+    const usersCollectionRef = collection(db, 'users');
+
+    const createUser = async () => {
+        await addDoc(usersCollectionRef, { name: getNewName });
+    }
+    const editUser = async (id) => {
+        const userDoc = doc(db, "users", id);
+        const newName = { name: getNewName };
+        await updateDoc(userDoc, newName);
+    }
+    const deleteUser = async (id) => {
+        const userDoc = doc(db, "users", id);
+        await deleteDoc(userDoc);
+    }
+
+    useEffect(() => {
+        const getUsers = async () => {
+            const data = await getDocs(usersCollectionRef);
+            setUser(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
+        }
+        getUsers();
+    }, [])
+    return (
+        <div className="App">
+            <input placeholder='Name...' onChange={(event) => { setNewName(event.target.value) }}></input>
+            <button onClick={createUser}>Submit</button>
+            {users.map((user) => { return <div><h1 key={user.id}>Name:{user.name}</h1> <button onClick={() => { editUser(user.id) }}>Edit</button> <button onClick={() => { deleteUser(user.id) }}>Delete</button></div> })}
+        </div>
+    );
+}
+export default Authentication;
